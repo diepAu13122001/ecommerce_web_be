87488d621898b628de5e729c9cac52d3f8ffe25a
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-05-12 16:22:49 +0700

    first commit

---
+++ ecommerceweb/src/main/java/com/backend/ecommerceweb/EcommercewebApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ecommerceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercewebApplication.class, args);
+	}
+
+}
